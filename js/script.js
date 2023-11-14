df67f049a0cc407005609d8de713b91753d419b3
bprenticeIIT <143012056+bprenticeIIT@users.noreply.github.com>
2023-11-14 10:25:18 -0600

    Create script.js and write code

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function initMap(){
+var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.95535438394767, lng: -87.72210225143667},
+    zoom: 12
+  });
+
+var marker = new google.maps.Marker({
+    position: { lat: 41.95535438394767, lng: -87.72210225143667},
+    map: map,
+  });
+
+var infowindow = new google.maps.InfoWindow({
+    content:"This is where I work!"
+  });
+  infowindow.open(map, marker);
+  const icons = {
+      bird: {
+        icon: "media/garden_center_picture.jpg",
+      },
+    }
+    for (let i = 0; i < features.length; i++) {
+      const marker = new google.maps.Marker({
+        position: features[i].position,
+        icon: icons[features[i].type].icon,
+        map: map,
+      });
+    }
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
